3a59e8a9b55de59016179e3007ecb1b5ac3d9bed
AxeVillager <borre97@live.no>
2023-04-01 11:46:11 +0200

    Use snake case

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from caesar_cipher import encrypt
 
-plainText = 'Experience is the teacher of all things.'
+plain_text = 'Experience is the teacher of all things.'
 offset = 3
-cipher = encrypt(plainText, offset)
+cipher = encrypt(plain_text, offset)
 
 print('Caesar Cipher Demo:')
-print(f'Plain text: {plainText}')
+print(f'Plain text: {plain_text}')
 print(f'Offset: {offset}')
 print(f'Cipher: {cipher}')
